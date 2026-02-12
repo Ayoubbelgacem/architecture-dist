1b0de5105ee1eacf067e651e9a77bc77a7062285
Ayoubbelgacem <Ayoubbelgacim@gmail.com>
2026-02-12 01:52:09 +0100

    feat: ajout du microservice produit Spring Boot avec CRUD et H2

---
+++ Backend/src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import com.example.demo.model.Candidat;
+import com.example.demo.repository.CandidatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+
+
+    @Autowired
+    private CandidatRepository repository;
+
+    @Bean
+    ApplicationRunner init() {
+        return (args) -> {
+// save
+            repository.save(new Candidat("Mariem", "Ch", "ma@esprit.tn"));
+            repository.save(new Candidat("Sarra", "ab", "sa@esprit.tn"));
+            repository.save(new Candidat("Mohamed", "ba", "mo@esprit.tn"));
+            repository.save(new Candidat("Maroua", "dh", "maroua@esprit.tn"));
+// fetch
+            repository.findAll().forEach(System.out::println);
+
+
+        };
+    }
+}
---
+++ Backend/src/main/java/com/example/demo/model/Candidat.java
@@ -0,0 +1,62 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Candidat implements Serializable {
+    private static final long serialVersionUID = 6L;
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nom, prenom, email;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Candidat() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public Candidat(String nom) {
+        super();
+        this.nom = nom;
+    }
+    public Candidat(String nom,String prenom,String  email) {
+        super();
+        this.email = email;
+        this.prenom = prenom;
+        this.nom = nom;
+    }
+}
